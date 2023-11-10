383f0881a896776eb078b907521d190f760b1940
Volodymyr <vovadrosenko@gmail.com>
2023-11-10 23:39:51 +0200

    Data base connection. Created User Model in Core Commit All and push

---
+++ TaskBobkoManager.Infrastructure/ServiceExtention.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBobkoManager.Infrastructure
+{
+    public static class ServiceExtention
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<DBContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+    }
+}
